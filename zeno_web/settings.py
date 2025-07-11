15c6e67bc1b24eaf03f0fca657561998d7a59e08
parv <parv@rhour.com>
2025-07-11 14:56:16 +0530

    Migrated from sqlite to postgres

--- zeno_web/settings.py
+++ zeno_web/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'zeno_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'zeno_db',
+        'USER': 'zenoadmin',
+        'PASSWORD': 'zenoadmin@2025',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
