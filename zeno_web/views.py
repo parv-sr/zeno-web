1407295e71c748baf2a066e9340800762742bc5f
parv <parv@rhour.com>
2025-07-25 19:23:54 +0530

    UI/UX enhancements on About Us page

--- zeno_web/views.py
+++ zeno_web/views.py
@@ -7,7 +7,23 @@ def home_view(request):
 
 def about_us_view(request):
     subjects = Subject.objects.all()
-    return render(request, 'about_us.html', {'subjects':subjects})
+
+    subject_icon_map = {
+        "Math (inclusive of mechanics, pure math and statistics) (8-12)": "Maths.svg",
+        "Business (9-12)": "Business 2.svg",
+        "IGCSE EFL and FLE (9-10)": "FLE_ESL.svg",
+        "Environmental science (9-12)": "EVM.svg",
+        "EGP": "EGP.svg",
+        "Biology (8-12)": "Biology.svg",
+        "Economics (9-12)": "Econ.svg",
+        "Chemistry (8-12)": "Chemistry.svg",
+        "Physics (8-12)": "Physics.svg",
+    }
+
+    return render(request, 'about_us.html', {
+        'subjects':subjects,
+        "subject_icons": subject_icon_map,
+        })
 
 def code_of_conduct(request):
     return render(request, 'code_of_conduct.html')
